3027e4b278264d42cd66bc390689d9c523d5cf4e
sobuj54 <sobujahmed124@gmail.com>
2025-07-17 15:17:29 +0600

    database configuration done

--- config/settings.py
+++ config/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default=os.getenv('DATABASE_URL'),
+        default=config('DATABASE_URL'),
         conn_max_age=600
     )
 }
